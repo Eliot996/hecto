5cabd74377b30b6e12ec71823145499ba5764bc8
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2022-12-24 00:12:04 +0100

    added to ctrl byte and now quits on ctrl + q

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,13 @@ fn main() {
         } else {
             println!("{:?} ({})\r", b, c);
         }
-        if c == 'q' {
+        if b == to_ctrl_byte('q') {
             break;
         }
     }
+}
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
 }
