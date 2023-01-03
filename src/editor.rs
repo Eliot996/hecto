d683ad528eae80e499b7714f347ed963e847ae47
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-01-03 16:04:44 +0100

    added cursor movement through arrow keys

--- src/editor.rs
+++ src/editor.rs
@@ -41,8 +41,8 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position{x:0, y:0});
-        
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
+
         if self.should_quit {
             Terminal::clear_screen();
             print!("Goodbye.\r");
@@ -59,9 +59,36 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
+    }
+
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut x, mut y } = &self.cursor_position;
+
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as u32;
+        let width = size.width.saturating_sub(1) as u32;
+
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1)
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1)
+                }
+            }
+            _ => (),
+        }
+
+        self.cursor_position = Position { x, y }
     }
 
     fn draw_welcome_message(&self) {
